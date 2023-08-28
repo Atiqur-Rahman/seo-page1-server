099c93cc7007521379e4e8b7678de305b59a2948
Atiqur-Rahman <rahmanatique88@gmail.com>
2023-08-28 10:27:58 +0600

    server is completed

---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
+const path = require('path');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './public/Images');
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + '_' + Date.now() + path.extname(file.originalname));
+    },
+});
+
+const upload = multer({ storage: storage });
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hhukhjd.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        const fileCollection = client.db('seo-page1').collection('files');
+
+        app.post('/upload', upload.array('file'), async (req, res) => {
+            const result = await fileCollection.insertMany(req.files);
+            res.send(`${result.insertedCount} documents were inserted`);
+        });
+
+        app.get('/upload', async (req, res) => {
+            const query = {};
+            const cursor = fileCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Running My Node CRUD Server ');
+});
+
+app.listen(port, () => {
+    console.log('server is running');
+});
